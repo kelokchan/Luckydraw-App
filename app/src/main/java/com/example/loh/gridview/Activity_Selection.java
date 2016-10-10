b4feb5503624cf0264ee61fb587263cb2e65a348
Kelok <kelokchan@gmail.com>
2016-10-10 14:43:04 +0800

    added permission request for >sdk23

--- app/src/main/java/com/example/loh/gridview/Activity_Selection.java
+++ app/src/main/java/com/example/loh/gridview/Activity_Selection.java
@@ -1,7 +1,11 @@
 package com.example.loh.gridview;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
 
@@ -22,10 +26,21 @@ public class Activity_Selection extends AppCompatActivity {
         setContentView(R.layout.activity_selection);
 
         getSupportActionBar().hide();
+
+        if (Build.VERSION.SDK_INT >= 23) {
+            //do your check here
+            if (checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(Activity_Selection.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+            }
+        }
+
+
         RingButton ringButton = (RingButton) findViewById(R.id.ringButton);
         ringButton.setOnClickListener(new RingButton.OnClickListener() {
             @Override
             public void clickUp() {
+
                 Intent intent = new Intent(Activity_Selection.this, Activity_Box.class);
                 startActivity(intent);
             }
@@ -37,17 +52,17 @@ public class Activity_Selection extends AppCompatActivity {
                     List<DivisionItem> divisionItemList = daOdb.getDivisionItems();
                     if (divisionItemList.size() < 1) {
                         Toast.makeText(Activity_Selection.this, "Previous settings not found", Toast.LENGTH_SHORT).show();
+
                         PhotoPickerIntent intent = new PhotoPickerIntent(Activity_Selection.this);
                         intent.setPhotoCount(16);
                         intent.setShowCamera(true);
                         intent.setShowGif(true);
                         startActivityForResult(intent, REQUEST_CODE);
-                    }else {
+                    } else {
                         Intent intent = new Intent(Activity_Selection.this, Activity_LuckyWheel.class);
                         startActivity(intent);
                     }
-                }
-                catch(Exception e){
+                } catch (Exception e) {
 
                 }
             }
@@ -72,5 +87,17 @@ public class Activity_Selection extends AppCompatActivity {
     @Override
     public void onBackPressed() {
         moveTaskToBack(true);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(grantResults[0]== PackageManager.PERMISSION_GRANTED){
+            //resume tasks needing this permission
+            Toast.makeText(this, "Permission granted", Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show();
+            finish();
+        }
     }
 }
